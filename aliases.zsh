959d0913927e401cc87e8899bc7a4735352ab422
Aaron <aaronjohndunphy@gmail.com>
2020-11-14 11:20:57 +0000

    Updated aliases

--- aliases.zsh
+++ aliases.zsh
@@ -7,15 +7,20 @@ alias emoji="code ~/.oh-my-zsh/themes/robbyrussell.zsh-theme --wait; source ~/.o
 # Git
 alias g="git add . && git commit -m $1"
 alias branches="git for-each-ref --count=15 --sort=-committerdate refs/heads/ --format='%(color:blue)%(committerdate:relative): %(color:green)%(refname:short)'"
+alias fcheck="git fetch && git checkout $1"
 
 # Laravel Artisan
 alias mf="php artisan migrate:fresh"
 alias mfs="php artisan migrate:fresh --seed"
 alias tinker="php artisan tinker"
 alias tink="php artisan tinker"
+alias migrate="php artisan migrate"
+alias rollback="php artisan migrate:rollback"
 
 # Laravel
 alias phpunit="vendor/bin/phpunit"
 alias insights="./vendor/bin/phpinsights"
 alias jigsaw="./vendor/bin/jigsaw"
-alias install="composer install && npm i && npm run dev"
+alias install="composer install && npm ci && npm run dev"
+alias testall="php artisan test && composer analyse && php artisan dusk"
+alias watch="npm run watch"
